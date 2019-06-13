620f57663459cbcdbf97caf68f2083e73a70bdda
Debaleena Paul <pdebale@americas.manulife.net>
2019-06-13 08:30:02 -0400

    Added serializers for the models

---
+++ qubeInfraSight/projectoffice/serializers.py
@@ -0,0 +1,51 @@
+from rest_framework import serializers
+from .models import Customer, CustomerCommChannel, Address, Project, CustomerAdditionalAttribute, Email, Phone, \
+    ProjectAttributes
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+
+
+class PhoneSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Phone
+        fields = '__all__'
+
+
+class CustomerCommChannelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomerCommChannel
+        fields = '__all__'
+
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = '__all__'
+
+
+class CustomerAdditionalInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomerAdditionalAttribute
+        fields = '__all__'
+
+
+class EmailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Email
+        fields = '__all__'
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+
+class ProjectAttributeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProjectAttributes
+        fields = '__all__'
